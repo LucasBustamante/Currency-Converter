6b355fc78ca51b3d5d848eaafb629eed433a2d77
Lucas Bustamante <lucascostabustamante@gmail.com>
2022-08-11 22:57:04 -0300

    arquitetura de software

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import'package:flutter/material.dart';
+
+const Color kButtonColor = Color(0xff19364C);
+const Color kPrimaryColor = Color(0xff1C3B53);
+const Color kContainer1Color = Color(0xff314D61);
+const Color kContainer2Color = Color(0xffACB4BA);
+const Color kBackgroundColor = Color(0xffe7e9ee);
